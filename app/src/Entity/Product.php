f6c7983db2660020ebffbd00d0fdeb7a5bf3c665
Shleif91 <shleikooleg@gmail.com>
2018-04-26 18:01:33 +0300

    crated Product and Offer entity

---
+++ app/src/Entity/Product.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ApiResource
+ * @ORM\Entity
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    public $id;
+
+    /**
+     * @ORM\Column
+     * @Assert\NotBlank()
+     */
+    public $name;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Offer", mappedBy="product", cascade={"persist"})
+     */
+    public $offers;
+
+    public function __construct()
+    {
+        $this->offers = new ArrayCollection(); // Initialize $offers as an Doctrine collection
+    }
+
+    public function addOffer(Offer $offer): void
+    {
+        $offer->product = $this;
+        $this->offers->add($offer);
+    }
+
+    public function removeOffer(Offer $offer): void
+    {
+        $offer->product = null;
+        $this->offers->removeElement($offer);
+    }
+}
